3f77eea8e5429f630835bde9ac2aa96948c9666e
Nolan Moss <140113152+Crusoe22@users.noreply.github.com>
2024-03-11 11:28:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import plotly.express as px
 from shiny.express import input, ui
-from shinywidgets import render_plotly, output_widget, render_widget
+from shinywidgets import render_plotly
 import palmerpenguins  # This package provides the Palmer Penguins dataset
 import pandas
 from shiny import render
@@ -10,6 +10,8 @@ import seaborn as sns
 penguins_df = palmerpenguins.load_penguins()
 
 ui.page_opts(title="Nolan's Penguin Data", fillable=True)
+
+# Plot Charts
 with ui.layout_columns():
 
     @render_plotly
@@ -20,76 +22,31 @@ with ui.layout_columns():
     def plot2():
         return px.histogram(px.data.tips(), y="total_bill")
 
-# Add a Shiny UI sidebar for user interaction
-# Use the ui.sidebar() function to create a sidebar
-# Set the open parameter to "open" to make the sidebar open by default
-# Use a with block to add content to the sidebar
-
+#Shiny UI sidebar for user interaction
 with ui.sidebar(open="open"):
-
-# Use the ui.h2() function to add a 2nd level header to the sidebar
-#   pass in a string argument (in quotes) to set the header text to "Sidebar"
     ui.h2("Sidebar")
     
-# Use ui.input_selectize() to create a dropdown input to choose a column
-#   pass in three arguments:
-#   the name of the input (in quotes), e.g., "selected_attribute"
-#   the label for the input (in quotes)
-#   a list of options for the input (in square brackets) 
-#   e.g. ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g"]
+    # Create a dropdown input to choose a column 
+    ui.input_selectize("selected_attribute", "Body Measurement in Millimeters", 
+                       ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g"]) 
     
-    ui.input_selectize("selected_attribute", "Body Measurement in Millimeters", ["bill_length_mm", 
-                                                                                 "bill_depth_mm", 
-                                                                                 "flipper_length_mm", 
-                                                                                 "body_mass_g"]) #Maybe change categories
-
-
-    
-# Use ui.input_numeric() to create a numeric input for the number of Plotly histogram bins
-#   pass in two arguments:
-#   the name of the input (in quotes), e.g. "plotly_bin_count"
-#   the label for the input (in quotes)
-
+    # Create a numeric input for the number of Plotly histogram bins
     ui.input_numeric("plotly_bin_count", "Bin Count", 10)
-
     
-# Use ui.input_slider() to create a slider input for the number of Seaborn bins
-#   pass in four arguments:
-#   the name of the input (in quotes), e.g. "seaborn_bin_count"
-#   the label for the input (in quotes)
-#   the minimum value for the input (as an integer)
-#   the maximum value for the input (as an integer)
-#   the default value for the input (as an integer)
-    ui.input_slider("seaborn_bin_count", "Label for Input", 1, 100, 50)
+    # Create a slider input for the number of Seaborn bins
+    ui.input_slider("seaborn_bin_count", "Bin Count", 1, 100, 50)
 
-# Use ui.input_checkbox_group() to create a checkbox group input to filter the species
-#   pass in five arguments:
-#   the name of the input (in quotes), e.g.  "selected_species_list"
-#   the label for the input (in quotes)
-#   a list of options for the input (in square brackets) as ["Adelie", "Gentoo", "Chinstrap"]
-#   a keyword argument selected= a list of selected options for the input (in square brackets)
-#   a keyword argument inline= a Boolean value (True or False) as you like
+    # Create a checkbox group input to filter the species
+    ui.input_checkbox_group("selected_species_lis", "Selected Species of Penguins", 
+                            ["Adelie", "Gentoo", "Chinstrap"], selected="", inline=False)
 
-    ui.input_checkbox_group("selected_species_lis", "Selected Species of Penguins", ["Adelie", "Gentoo", "Chinstrap"], selected="", inline=False)
-
-
-    
-# Use ui.hr() to add a horizontal rule to the sidebar
-
+    # Add a horizontal rule to the sidebar
     ui.hr()
 
-# Use ui.a() to add a hyperlink to the sidebar
-#   pass in two arguments:
-#   the text for the hyperlink (in quotes), e.g. "GitHub"
-#   a keyword argument href= the URL for the hyperlink (in quotes), e.g. your GitHub repo URL
-#   a keyword argument target= "_blank" to open the link in a new tab
-
+    # Add a hyperlink to the sidebar
     ui.a("GitHub", href="https://github.com/Crusoe22/cintel-02-data.git", target="_blank")
 
-
-# When passing in multiple arguments to a function, separate them with commas.
-
-
+# Show Data
 with ui.layout_columns():
 
     @render.data_frame
@@ -100,6 +57,7 @@ with ui.layout_columns():
     def penguins_grid():
         return render.DataGrid(penguins_df)
 
+# Plot Charts
 with ui.layout_columns():
     @render_plotly  
     def plot_plt():  
@@ -112,7 +70,7 @@ with ui.layout_columns():
     def plot_sns():  
         return sns.histplot(penguins_df, x="species", kde=False)
 
-
+#Create Scatter plot
 with ui.card(full_screen=True):
 
     ui.card_header("Plotly Scatterplot: Species")
@@ -120,8 +78,6 @@ with ui.card(full_screen=True):
     @render_plotly
     def plotly_scatterplot():
         # Create a Plotly scatterplot using Plotly Express
-        # Call px.scatter() function
-        # Pass in six arguments:
         return px.scatter(penguins_df, x="flipper_length_mm", y="bill_length_mm", color="species", 
                           facet_row="species", facet_col="sex", title="Penguin Scatterplot", labels={"flipper_length_mm": "Flipper Length (mm)", "bill_length_mm": "Bill Length (mm)"})
 
